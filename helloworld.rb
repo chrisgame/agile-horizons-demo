db72b5c4060b7307a6c1bc3f42839021c37b7473
Chris Game <chris.game@robbiebobbins.com>
2012-04-29 14:56:10 +0100

    test

--- helloworld.rb
+++ helloworld.rb
@@ -1,5 +1,32 @@
 require 'sinatra'
+require 'haml'
+require 'airbrake'
 
-get '/' do
-  'Hello World'
+Airbrake.configure do |config|
+  config.api_key = ENV['AIRBRAKE_API_KEY']
 end
+
+get '/?' do
+  "This is the #{ENV['RACK_ENV']} environment"
+end
+
+get '/error/?' do
+  haml :error_generator
+end
+
+get '/generate-error/?' do
+  raise 'Someone pressed the button'
+  redirect '/error'
+end
+
+__END__
+
+@@ layout
+%html
+  %body{:style => 'text-align: center;'}
+    = yield
+
+@@ error_generator
+%p{:style => 'font-weight: bold;'}
+  Don't Press This Button
+%a{:style => 'background: url("/red_button.jpeg") no-repeat center center; display: block; height: 225px;', :href => '/generate-error'}
